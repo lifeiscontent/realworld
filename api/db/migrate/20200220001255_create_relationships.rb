826d3f6fc8405f3bb3816c0015d228fd3ea5fd9c
Aaron Reisman <aaron@lifeiscontent.net>
2020-02-24 13:34:10 -0800

    Fix Relationships

--- api/db/migrate/20200220001255_create_relationships.rb
+++ api/db/migrate/20200220001255_create_relationships.rb
@@ -3,8 +3,8 @@
 class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
-      t.belongs_to :followed, null: false
-      t.belongs_to :follower, null: false
+      t.belongs_to :followed, null: false, foreign_key: { to_table: :users }
+      t.belongs_to :follower, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
