c9343ed61a2209453cf19b3d0366bae1c0202fc8
Aaron Reisman <aaron@lifeiscontent.net>
2020-03-13 18:41:51 -0700

    Remove useless code

--- web/src/pages/_app.js
+++ web/src/pages/_app.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import NextApp from 'next/app';
-import getShellLayout from '../layouts/shell';
 
 class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props;
 
-    const getLayout = Component.getLayout || getShellLayout;
-
-    return getLayout(<Component {...pageProps} />);
+    return <Component {...pageProps} />;
   }
 }
 
