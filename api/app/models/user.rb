0571b8e10fdc71473aca40d5b0cce27316fba346
Aaron Reisman <aaron@lifeiscontent.net>
2020-03-15 23:15:15 -0700

    Fix secret_key_base

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   def self.from_jwt(token)
     jwt_payload = JWT.decode(
       token,
-      Rails.application.secrets.secret_key_base
+      Rails.application.credentials.dig(:secret_key_base)
     ).first
     User.find(jwt_payload['id'])
   rescue JWT::ExpiredSignature,
@@ -41,7 +41,7 @@ class User < ApplicationRecord
       {
         id: id,
         exp: 24.hours.from_now.to_i
-      }, Rails.application.secrets.secret_key_base
+      }, Rails.application.credentials.dig(:secret_key_base)
     )
   end
 
