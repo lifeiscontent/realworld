95579d86d3f568fd28ae62f1670958978bf1c77c
Aaron Reisman <aaron@lifeiscontent.net>
2020-03-15 22:51:35 -0700

    Remove creds

--- web/src/apollo/index.js
+++ web/src/apollo/index.js
@@ -15,7 +15,6 @@ export default function createApolloClient(initialState, ctx) {
     link: new HttpLink({
       headers: ctx?.req?.headers,
       fetch,
-      credentials: 'include',
       uri: process.env.GRAPHQL_URL
     }),
     cache: new InMemoryCache(cacheConfig).restore(initialState)
