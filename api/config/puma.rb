a3f8e6e7283dd55e4d66ce6400c09ea8aae40300
Aaron Reisman <aaron@lifeiscontent.net>
2020-03-15 18:38:29 -0700

    Fix Puma

--- api/config/puma.rb
+++ api/config/puma.rb
@@ -18,7 +18,7 @@ port        ENV.fetch('PORT') { 4000 }
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
+# pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
