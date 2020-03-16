59c8a3f4af6805a722ee25ab6d2d960d1da0faa1
Aaron Reisman <aaron@lifeiscontent.net>
2020-03-15 23:32:56 -0700

    Fix Logout Page

--- web/src/pages/logout.js
+++ web/src/pages/logout.js
@@ -2,15 +2,17 @@ import Router from 'next/router';
 import { useEffect } from 'react';
 
 export default function Logout() {
-  document.cookie = cookie.serialize('authorization', '', {
-    maxAge: -1,
-    path: '/',
-    sameSite: 'lax',
-    secure: process.env.NODE_ENV === 'production'
-  });
-
   useEffect(() => {
-    Router.replace('/', '/');
+    if (typeof window !== 'undefined') {
+      document.cookie = cookie.serialize('authorization', '', {
+        maxAge: -1,
+        path: '/',
+        sameSite: 'lax',
+        secure: process.env.NODE_ENV === 'production'
+      });
+
+      Router.replace('/', '/');
+    }
   }, []);
 
   return null;
