2e4307564276d831bfdc64c314a18d6dfbf9a08f
Aaron Reisman <aaron@lifeiscontent.net>
2020-03-15 22:44:19 -0700

    Fix API URLs

--- web/next.config.js
+++ web/next.config.js
@@ -2,7 +2,6 @@ module.exports = {
   env: {
     GRAPHQL_URL_SERVER:
       'https://realworld-api-production.herokuapp.com/graphql',
-    GRAPHQL_URL_CLIENT:
-      'https://realworld-web-production.herokuapp.com/api/graphql'
+    GRAPHQL_URL_CLIENT: 'https://realworld.now.sh/api/graphql'
   }
 };
