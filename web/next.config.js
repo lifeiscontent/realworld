297adfc96179f49794a95cf4a1ad05981aaf5d31
Aaron Reisman <aaron@lifeiscontent.net>
2020-03-18 13:57:21 -0700

    Make env configurable

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,22 @@
-module.exports = {
-  env: {
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+const env = [
+  '.env',
+  `.env.${process.env.NODE_ENV}`,
+  `.env.${process.env.NODE_ENV}.local`
+].reduce(
+  (env, file) => {
+    if (fs.existsSync(file)) {
+      return { ...env, ...dotenv.parse(fs.readFileSync(file)) };
+    }
+    return env;
+  },
+  {
     GRAPHQL_URL: 'https://realworld-api-production.herokuapp.com/graphql'
   }
+);
+
+module.exports = {
+  env
 };
