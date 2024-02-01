f57d491ca77afdaff845c6437e62cabf95757df2
dulao5 <dulao5@gmail.com>
2024-02-01 19:55:09 +0900

    to fix Mysql2::Error: BLOB/TEXT/JSON column 'bio' can't have a default value

--- api/db/migrate/20200219235616_create_profiles.rb
+++ api/db/migrate/20200219235616_create_profiles.rb
@@ -3,7 +3,7 @@
 class CreateProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :profiles do |t|
-      t.text :bio, null: false, default: ''
+      t.text :bio, null: true
       t.string :image_url
       t.belongs_to :user, null: false, index: { unique: true }, foreign_key: true
       t.timestamps
