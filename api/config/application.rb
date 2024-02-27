52e35f8e93c73d6cfe68964d6794a9e06224a241
dulao5 <dulao5@gmail.com>
2024-02-27 11:49:22 +0900

    support proxysql (sidecar mode)

--- api/config/application.rb
+++ api/config/application.rb
@@ -16,6 +16,7 @@ require 'action_view/railtie'
 #require 'action_cable/engine'
 # require "sprockets/railtie"
 require 'rails/test_unit/railtie'
+require "action_cable"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
